98ffd21d067ee411b64ae6d4f4c0144e8e858897
RachelNolan <mail@rachelnolan.com>
2014-10-22 11:12:46 +0100

    Thermostat has Power Saving Mode turned on by default

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,15 +1,22 @@
 
 describe('Thermostat', function () {
 
-	var = thermostat();
+	// var = thermostat();
 
 	describe('by default', function() {
 
-		it('temperature should be set to 20 degrees', function() { 
+		beforeEach(function(){
 			thermostat = new Thermostat;
+		});
+
+		it('temperature should be set to 20 degrees', function() { 
 			expect(thermostat.temperature).toEqual(20);
 
 		});
+
+		it('should have power saving mode turned on', function() {
+			expect(thermostat.isPowerSavingModeOn).toBe(true)
+		}); 
 
 	});
 
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,4 @@
 function Thermostat() {
 	this.temperature = 20;
+	this.isPowerSavingModeOn = true;
 };
